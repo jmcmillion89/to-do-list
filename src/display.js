4f757fb80880278dfc16fe89b0262a62e938c9b2
Justin <jmcmillion89@icloud.com>
2026-02-07 17:29:56 -0500

    Add function for todo card

--- src/display.js
+++ src/display.js
@@ -1,7 +1,52 @@
 import { projects } from "./projects.js"
+import { toDos } from "./todos.js"
 
 export const renderDisplay = () => {
     buildProjectList()
+}
+
+const changeBackground = (element) => {
+    const parentElement = element.parentNode
+    parentElement.style.background = 'red'
+
+}
+
+const displayComplete = (todo) => {
+    let complete = todo.complete === true ? '✔' : '✘';
+    return complete
+}
+
+const changeComplete = (button, todo) => {
+    todo.updateComplete()
+    button.textContent = displayComplete(todo)
+    changeBackground(button)
+}
+
+const displayToDos = (project) => {
+    const toDosList = document.querySelector('.todos')
+    toDosList.textContent = ''
+    const filteredToDos = toDos.filter((todo) => todo.project === project)
+    filteredToDos.forEach((todo) => {
+        const newDiv = document.createElement('div')
+        newDiv.classList.add('todo')
+        const titleDiv = document.createElement('div')
+        titleDiv.textContent = todo.title
+        newDiv.appendChild(titleDiv)
+        const dueDateDiv = document.createElement('div')
+        dueDateDiv.textContent = todo.dueDate
+        newDiv.appendChild(dueDateDiv)
+        const priorityDiv = document.createElement('div')
+        priorityDiv.textContent = todo.priority
+        newDiv.appendChild(priorityDiv)
+        const completeBtn = document.createElement('button')
+        completeBtn.classList.add('complete-button')
+        completeBtn.textContent = displayComplete(todo)
+        completeBtn.addEventListener(('click'), (e) => {
+            changeComplete(e.target, todo)
+        })
+        newDiv.appendChild(completeBtn)
+        toDosList.appendChild(newDiv)
+    })
 }
 
 const buildProjectList = () => {
@@ -9,6 +54,10 @@ const buildProjectList = () => {
     projects.forEach((project) => {
         const newLi = document.createElement('li')
         newLi.textContent = project
+        newLi.addEventListener('click', () => {
+            displayToDos(project)
+        })
         projectNav.appendChild(newLi)
     })
 }
+
