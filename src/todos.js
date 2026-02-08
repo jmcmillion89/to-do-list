ae791c6478f7a48706917a05ba1af68aafb8c0d4
Justin <jmcmillion89@icloud.com>
2026-02-07 22:45:30 -0500

    Add function to remove todo

--- src/todos.js
+++ src/todos.js
@@ -8,6 +8,7 @@ class toDo {
         this.priority = priority
         this.project = project
         this.complete = false
+        this.id = crypto.randomUUID();
     }
 
     updateComplete() {
@@ -19,3 +20,28 @@ class toDo {
 export const addNewToDo = (title, description, dueDate, priority, project) => {
     toDos.push(new toDo(title, description, dueDate, priority, project))
 }
+
+const findIndex = (id) => {
+    let value;
+    toDos.forEach((toDo, index) => {
+        if (toDo.id === id) {
+            value = index
+        }
+    }
+)
+    return value
+}
+
+export const removeToDo = (id) => {
+    let index = findIndex(id)
+    toDos.splice(index, 1)
+}
+
+
+
+addNewToDo('test title1', 'test description1', '01/01/2027', 'Medium', 'Default')
+addNewToDo('test title2', 'test description2', '01/01/2027', 'High', 'Default')
+addNewToDo('test title3', 'test description3', '01/01/2027', 'Medium', 'Default')
+addNewToDo('test title4', 'test description4', '01/01/2027', 'Low', 'Default')
+addNewToDo('test title5', 'test description5', '01/01/2027', 'Medium', 'Default')
+addNewToDo('test title6', 'test description6', '01/01/2027', 'High', 'Default')
