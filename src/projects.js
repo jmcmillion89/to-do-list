e409e85e0c17de6acae8b162e4bfa4f6e7cfb6d7
Justin <jmcmillion89@icloud.com>
2026-02-08 17:05:58 -0500

    Add function to call remove ToDos function

--- src/projects.js
+++ src/projects.js
@@ -1,3 +1,5 @@
+import { removeProjectToDos } from "./todos.js"
+
 export let projects = ['Default']
 
 export const addProject = (projectName) => {
@@ -9,4 +11,5 @@ export const addProject = (projectName) => {
 export const removeProject = (projectName) => {
     let index = projects.indexOf(projectName)
     projects.splice(index, 1)
+    removeProjectToDos(projectName)
 }
