d9c7f140cc316d124718af62ebae5dbcad4eb9a9
Justin <jmcmillion89@icloud.com>
2026-02-15 10:43:41 -0500

    Add localstorage for projects

--- src/projects.js
+++ src/projects.js
@@ -1,15 +1,34 @@
+const key = 'projects'
+export const savedProjects = JSON.parse(localStorage.getItem(key))
+
 import { removeProjectToDos } from "./todos.js"
 
-export let projects = ['Default']
+export let projects = savedProjects === null ? ['Default'] : savedProjects
+
+const populateProjects = (() => {
+
+    if (savedProjects !== null) {
+        projects = []
+        savedProjects.forEach((project) => {
+        projects.push(project)
+    })
+    }
+})()
+
+const updateLocalStorage = () => {
+    localStorage.setItem(key, JSON.stringify(projects))
+}
 
 export const addProject = (projectName) => {
     if (projectName !== '') {
         projects.push(projectName)
+        updateLocalStorage()
     }
 }
 
 export const removeProject = (projectName) => {
     let index = projects.indexOf(projectName)
     projects.splice(index, 1)
+    updateLocalStorage()
     removeProjectToDos(projectName)
 }
