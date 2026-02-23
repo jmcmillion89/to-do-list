e74c6fdf8a348ec517d192570f6169285cfaf622
Justin <jmcmillion89@icloud.com>
2026-02-23 04:39:06 -0500

    Update local storage when adding new todo

--- src/todos.js
+++ src/todos.js
@@ -1,4 +1,4 @@
-import { getProjects } from "./projects.js"
+import { getProjects, updateLocalStorage } from "./projects.js"
 const projects = getProjects()
 
 class toDo {
@@ -18,7 +18,7 @@ const getProjectIndex = (id) => {
 }
 
 const getToDoIndex = (index, id) => {
-    return projects[index].todos.map(() => {return id}).indexOf(id)
+    return projects[index].todos.map((todo) => {return todo.id}).indexOf(id)
 
 }
 
@@ -30,23 +30,12 @@ export const addNewToDo =
     priority) => {
     const index = getProjectIndex(id)
     projects[index].todos.push(new toDo(title, description, dueDate, priority))
+    updateLocalStorage()
 }
 
 export const removeThisToDo = (projectID, toDoID) => {
     const projectIndex = getProjectIndex(projectID)
     const toDoIndex = getToDoIndex(projectIndex, toDoID)
     projects[projectIndex].todos.splice(toDoIndex, 1)
+    updateLocalStorage()
 }
-
-// const testID = () => {
-//     return projects[0].id
-// }
-
-// trying to figure out why my index is showing -1 instead of 0
-
-// addNewToDo(testID(), 'test title1', 'test description1', '01/01/2027', 'Medium')    
-// addNewToDo('test title2', 'test description2', '01/01/2027', 'High', 'Default')
-// addNewToDo('test title3', 'test description3', '01/01/2027', 'Medium', 'Default')
-// addNewToDo('test title4', 'test description4', '01/01/2027', 'Low', 'Default')
-// addNewToDo('test title5', 'test description5', '01/01/2027', 'Medium', 'Default')
-// addNewToDo('test title6', 'test description6', '01/01/2027', 'High', 'Default')
